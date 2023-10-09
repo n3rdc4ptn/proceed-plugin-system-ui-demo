1a282c829b8484bd1644d0ac7ace6b3fc2ae7314
Moritz Reich <moritz.reich@minest.de>
2023-10-09 12:27:31 +0200

    made the manifest bundle way working

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { DynamicComponent } from "../lib/components/dynamic/DynamicComponent";
+import { DynamicComponent } from "proceed-frontend-plugin-system";
 import { environment } from "../lib/environment";
 
 export default function Home() {
   const [plugins, setPlugins] = useState<string[]>([]);
 
-  // Dynamically fetch plugins from the server one Time on first render
   useEffect(() => {
     fetch(`${environment.pluginServerUrl}/plugins/`)
       .then((response) => response.json())
@@ -18,13 +17,19 @@ export default function Home() {
     <div>
       <h1>UI Plugin Demo</h1>
 
+      {/* <DynamicComponent
+        url={`${environment.pluginServerUrl}/plugins/com.example.plugin1/remoteEntry.js`}
+        scope="Plugin1"
+        module="./Module"
+      /> */}
+
       {plugins.map((plugin, index) => (
         <div key={index} className="my-4 p-4">
-          <p className="text-xl">{plugin}</p>
+          <p className="text-xl">{plugin.name}</p>
           <div className="border-black rounded-lg border-2 p-2">
             <DynamicComponent
-              url={`${environment.pluginServerUrl}/plugins/${plugin}/remoteEntry.js`}
-              scope={plugin}
+              url={`${environment.pluginServerUrl}/plugins/${plugin.bundle}/remoteEntry.js`}
+              scope={plugin.name}
               module="./Module"
             />
           </div>
