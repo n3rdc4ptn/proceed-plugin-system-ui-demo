f04de4059b6b598da9f826e46367121bd1ea7c27
Moritz Reich <moritz.reich@minest.de>
2024-01-29 17:32:31 +0100

    Remove unused code and update ExtensionPoint usage

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ import { useState } from "react";
 import { ExtensionPoint } from "proceed-frontend-plugin-system";
 
 export default function Home() {
-  const [plugins, setPlugins] = useState<string[]>([]);
-
   return (
     <div>
       <h1>UI Plugin Demo</h1>
 
-      <ExtensionPoint extensionPoint="demo" />
-      <ExtensionPoint extensionPoint="processlistview" />
+      <ExtensionPoint extensionPoint="demo" data={null} />
     </div>
   );
 }
